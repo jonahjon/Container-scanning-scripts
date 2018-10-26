d970c36bb52ba9c74caec6c44d5970d848c5551f
Jonah Jones <34656423+jonahjones777@users.noreply.github.com>
2018-10-26 18:17:38 -0400

    Create docker-bench.sh

---
+++ docker-bench.sh
@@ -0,0 +1,10 @@
+git clone https://github.com/docker/docker-bench-security.git
+cd docker-bench-security
+docker build -t docker-bench-security .
+docker run -it --net host --pid host --cap-add audit_control \
+    -e DOCKER_CONTENT_TRUST=$DOCKER_CONTENT_TRUST \
+    -v /var/lib:/var/lib \
+    -v /var/run/docker.sock:/var/run/docker.sock \
+    -v /usr/lib/systemd:/usr/lib/systemd \
+    -v /etc:/etc --label docker_bench_security \
+    docker-bench-security
